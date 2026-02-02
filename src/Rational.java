d9e08671dec8ef373501a48ab77261b486708994
bmwasser <you@examplegit config --global user.name Your>
2026-02-02 14:33:36 -0600

    Updated Java Doc comments for rational class

--- src/Rational.java
+++ src/Rational.java
@@ -14,8 +14,8 @@ public class Rational{
 
     /**
      * Creates a rational number by taking a numerator and denominator
-     * @param num
-     * @param denom
+     * @param num - numerator for the rational number
+     * @param denom - denominator for the rational number
      */
     public Rational(int num, int denom) {
         this.numerator = num;
@@ -25,7 +25,7 @@ public class Rational{
 
     /**
      * Creates a rational number by taking a numerator and putting it over 1
-     * @param num
+     * @param num - numerator for rational number that will be set over 1
      */
     public Rational(int num) {
         this.numerator = num;
@@ -58,7 +58,7 @@ public class Rational{
     /**
      * Formats the numerators and denominators that were given by the user into a 
      * format that is able to be printed as a String
-     * @return String
+     * @return the correct formatted String of the rational value which is numerator over denominator
      */
     public String toString() {
         return this.numerator + "/" + this.denominator;
@@ -66,7 +66,7 @@ public class Rational{
 
     /**
      * Converts the rational number into a double 
-     * @return double
+     * @return double of the rational value that was given which is a decimal
      */
     public double toDouble() {
         return (double) this.numerator / this.denominator;
@@ -74,7 +74,7 @@ public class Rational{
 
     /**
      * Inverts the rational number by flipping the numerator and denominator of given
-     * @return Rational
+     * @return Rational of inverted values of numerator and denominator 
      */
     public Rational invert() {
         return new Rational(this.denominator, this.numerator);
@@ -83,8 +83,8 @@ public class Rational{
     /**
      * Checks to see if the numerator and denominator of each rational number 
      * are equal to each other
-     * @param other
-     * @return boolean
+     * @param other - a different rational value which will be used to compare numerator and denominator
+     * @return boolean of true or false of whether the two rational values are equal
      */
     public boolean equals(Rational other){
         return (this.numerator == other.numerator) && (this.denominator == other.denominator);
@@ -93,8 +93,8 @@ public class Rational{
     /**
      * Adds the two rational numbers given by giving both like denominators
      * and then adding them together to create a sum
-     * @param other
-     * @return Rational
+     * @param other - a different rational number that will added along with the other rational
+     * @return Rational sum of the two rational numbers thaat were given
      */
     public Rational add(Rational other) {
         int addNum = this.numerator * other.denominator + this.denominator * other.numerator;
@@ -107,8 +107,8 @@ public class Rational{
     /**
      * Subtracts two rational numbers from each other by giving them like denominators
      * and then subtracting them together to create a difference
-     * @param other
-     * @return Rational
+     * @param other - a different rational value that will be subtracted from the original rational value
+     * @return Rational of the difference between the two rational numbers
      */
     public Rational subtract(Rational other) {
         int subNum = this.numerator * other.denominator - this.denominator * other.numerator;
@@ -121,8 +121,8 @@ public class Rational{
     /**
      * Takes the two rational numbers given and multiplies the two numerators and denominators
      * together creating a product
-     * @param other
-     * @return Rational
+     * @param other - a different rational number that will be multiplied by the other
+     * @return Rational product of the two different rational values
      */
     public Rational multiply(Rational other) {
         int multiNum = this.numerator * other.numerator;
@@ -135,8 +135,8 @@ public class Rational{
     /**
      * Takes the two rational numbers and inverts the second rational number that was sent as a parameter
      * then multiplying the numerator and denominator of both rationals
-     * @param other
-     * @return Rational
+     * @param other - a different rational number that will be flipped into a reciprocal
+     * @return Rational quotient of the original rational value and the reciprocal of the parameter rational value
      */
     public Rational divide(Rational other) {
         int divideNum = this.numerator * other.denominator;
